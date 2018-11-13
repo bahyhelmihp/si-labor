d6c1d0d2ec43a0ee73ab8152ff03cfe84cd88264
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-11-13 16:21:00 +0700

    Initiate Project

---
+++ src/main/java/com/apap/silabor/SiLaboratoriumApplication.java
@@ -0,0 +1,12 @@
+package com.apap.silabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiLaboratoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiLaboratoriumApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apap/silabor/SiLaboratoriumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apap.silabor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiLaboratoriumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
