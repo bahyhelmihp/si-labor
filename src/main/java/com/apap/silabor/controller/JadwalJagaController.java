b8994fd90db2316a6cca8d6ec8074c00930a91a7
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-11-14 15:41:57 +0700

    Progress jadwal_jaga

---
+++ src/main/java/com/apap/silabor/controller/JadwalJagaController.java
@@ -0,0 +1,8 @@
+package com.apap.silabor.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JadwalJagaController {
+
+}
--- src/main/java/com/apap/silabor/repository/JadwalJagaDb.java
+++ src/main/java/com/apap/silabor/repository/JadwalJagaDb.java
@@ -1,11 +1,16 @@
 package com.apap.silabor.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.apap.silabor.model.JadwalJagaModel;
 import com.apap.silabor.model.UserRoleModel;
 
 @Repository
-public interface JadwalJagaDb extends JpaRepository<UserRoleModel, Long> {
+public interface JadwalJagaDb extends JpaRepository<JadwalJagaModel, Long> {
+	
+	JadwalJagaModel findByDate (Date tanggal);
 	
 }
