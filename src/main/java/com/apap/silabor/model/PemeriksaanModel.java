6b05cd43bc5a45e42d0e2508334d0f95a942cc46
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-11-15 22:02:15 +0700

    Pemeriksaan Model Fixing

--- src/main/java/com/apap/silabor/model/PemeriksaanModel.java
+++ src/main/java/com/apap/silabor/model/PemeriksaanModel.java
@@ -5,12 +5,20 @@ import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pemeriksaan")
@@ -33,9 +41,21 @@ public class PemeriksaanModel implements Serializable {
 	private int status;
 	
 	@NotNull
+	@Column(name = "id_pasien", nullable = false)
+	private int pasien;
+	
+	@NotNull
 	@Size(max=255)
 	@Column(name= "hasil", nullable = false)
 	private String hasil;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "id_jenis", referencedColumnName = "id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	private JenisPemeriksaanModel jenis_pemeriksaan;
+	
+	
 }
 
 
