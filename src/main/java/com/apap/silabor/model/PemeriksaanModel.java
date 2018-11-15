f66c030f9d2f335677cf694212b605b1789d7247
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-11-15 20:55:32 +0700

    Add Pemeriksaan Model

--- src/main/java/com/apap/silabor/model/PemeriksaanModel.java
+++ src/main/java/com/apap/silabor/model/PemeriksaanModel.java
@@ -1,5 +1,40 @@
 package com.apap.silabor.model;
 
-public class PemeriksaanModel {
+import java.io.Serializable;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "pemeriksaan")
+public class PemeriksaanModel implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long id;
 	
+	@NotNull
+	@Column(name = "tanggal_pengajuan", nullable = false)
+	private Date tanggalPengajuan;
+	
+	@NotNull
+	@Column(name = "tanggal_pemeriksaan", nullable = false)
+	private Date tanggalPemeriksaan1;
+	
+	@NotNull
+	@Column(name = "status", nullable = false)
+	private int status;
+	
+	@NotNull
+	@Size(max=255)
+	@Column(name= "hasil", nullable = false)
+	private String hasil;
 }
+
