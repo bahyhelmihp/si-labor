f6d9e72a8acba19c2fa98361bb2d44474219b189
cvithasa <cvithasaik@gmail.com>
2018-11-21 02:39:44 +0700

    default value buat status, mencoba tanggal otomatis input

--- src/main/java/com/apap/silabor/controller/KebutuhanReagenController.java
+++ src/main/java/com/apap/silabor/controller/KebutuhanReagenController.java
@@ -2,7 +2,11 @@ package com.apap.silabor.controller;
 
 import com.apap.silabor.model.KebutuhanReagenModel;
 import com.apap.silabor.service.KebutuhanReagenService;
+import com.apap.silabor.model.SupplyModel;
+import com.apap.silabor.service.SupplyService;
 
+import java.time.LocalDate;
+import java.sql.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/*
+/**
  * KebutuhanReagenModel
  */
 @Controller
@@ -22,9 +26,24 @@ public class KebutuhanReagenController {
 	@Autowired
 	private KebutuhanReagenService kebutuhanReagenService;
 	
+	@Autowired
+	private SupplyService supplyService;
+	
+	//FITUR 3 : Membuat perencanaan kebutuhan reagen
 	@RequestMapping(value = "/lab/kebutuhan/tambah", method = RequestMethod.GET)
 	private String add(Model model) {
-		model.addAttribute("kebutuhan", new KebutuhanReagenModel());
+		KebutuhanReagenModel kebutuhan = new KebutuhanReagenModel();
+		
+		//gak masuk
+		LocalDate date = LocalDate.now();
+		Date tanggal =  Date.valueOf(date);
+		kebutuhan.setTanggal_update(tanggal);
+		
+		//id masih sementara
+		SupplyModel supply = supplyService.getSupplyById(1);
+		kebutuhan.setSupply(supply);
+		
+		model.addAttribute("kebutuhan", kebutuhan);
 		return "kebutuhan-add";
 	}
 	
