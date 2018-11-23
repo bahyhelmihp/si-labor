8424b0cda51f1b1fa3137688480392a564700d1f
zidane zaffar <zidane.zaffar@gmail.com>
2018-11-23 14:06:26 +0700

    making function for feature 7, not yet finished, still using dummy data

--- src/main/java/com/apap/silabor/controller/PemeriksaanController.java
+++ src/main/java/com/apap/silabor/controller/PemeriksaanController.java
@@ -1,18 +1,19 @@
 package com.apap.silabor.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.apap.silabor.model.PemeriksaanModel;
@@ -22,7 +23,7 @@ import com.apap.silabor.rest.PasienTest;
 import com.apap.silabor.rest.Setting;
 import com.apap.silabor.service.PemeriksaanService;
 
-@RestController
+@Controller
 @RequestMapping("/lab/pemeriksaan")
 public class PemeriksaanController {
 	@Autowired
@@ -40,12 +41,20 @@ public class PemeriksaanController {
 	//FITUR 7 Menampilkan permintaan pemeriksaan lab
 	@GetMapping(value = "/permintaan")
 	public String viewAllPemeriksaan(Model model) {
+		
+		// mengambil data dari {url}
+		String path = "aa";
+		List<Long> listOfIdPasien = new ArrayList<Long>();
+		//listOfIdPasien = restTemplate.getForObject(path, List.class);
+		
+		listOfIdPasien.add((long) 1);
+		listOfIdPasien.add((long) 2);
+		
 		List<PemeriksaanModel> listPemeriksaan = pemeriksaanService.getListPemeriksaan();
-		model.addAttribute("pemeriksaanList", listPemeriksaan);
-		model.addAttribute("title", "Daftar Pemeriksaan Lab");
+			model.addAttribute("pemeriksaanList", listPemeriksaan);
+			model.addAttribute("title", "Daftar Pemeriksaan Lab");
 		return "pemeriksaan-viewall";
 	}
-
 	//FITUR 8
 	@PostMapping(value = "/permintaan/save")
 	public PemeriksaanModel addPemeriksaan(@RequestBody PemeriksaanModel pemeriksaan) {
