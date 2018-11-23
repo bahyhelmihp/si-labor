ce58741405c421244a07fbfe24975c60ec837f8b
Bahy Helmi H P <bahy.helmi@outlook.com>
2018-11-23 09:56:38 +0700

    Fix Controller Logic

--- src/main/java/com/apap/silabor/controller/PemeriksaanController.java
+++ src/main/java/com/apap/silabor/controller/PemeriksaanController.java
@@ -5,14 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.apap.silabor.model.PemeriksaanModel;
@@ -22,7 +21,7 @@ import com.apap.silabor.rest.PasienTest;
 import com.apap.silabor.rest.Setting;
 import com.apap.silabor.service.PemeriksaanService;
 
-@RestController
+@Controller
 @RequestMapping("/lab/pemeriksaan")
 public class PemeriksaanController {
 	@Autowired
@@ -53,7 +52,7 @@ public class PemeriksaanController {
 	}
 
 	//FITUR 9
-	@GetMapping(value = "/{id}")
+	@PostMapping(value = "/{id}")
 	public long updateStatus(@PathVariable(value="id") long id) {
 		PemeriksaanModel pemeriksaan = pemeriksaanService.getPemeriksaanById(id);
 		return pemeriksaan.getId();
@@ -75,15 +74,8 @@ public class PemeriksaanController {
 		//Consume API
 		LabResponse response = restTemplate.postForObject(Setting.addLabResultUrl, labResult, LabResponse.class);
 		
-		//Return Response
-		return response.getMessage();
-	}
-
-	@GetMapping(value = "/tompel")
-	public ResponseEntity<String> getPasien() {
-		String path = "http://si-appointment.herokuapp.com/api/6/getAllPasienRawatJalan";
-		ResponseEntity<String> response = restTemplate.getForEntity(path, String.class);
-		return response;
+		//Return 
+		return "redirect:/lab/pemeriksaan/permintaan";
 	}
 
 }
