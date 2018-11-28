bb6fb0cfe4f8280d4d709c38f74f8707f12f325c
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-11-28 12:38:36 +0700

    Another for jadwalJaga

--- src/main/java/com/apap/silabor/controller/JadwalJagaController.java
+++ src/main/java/com/apap/silabor/controller/JadwalJagaController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,7 +37,7 @@ public class JadwalJagaController {
 	JadwalJagaService jadwalJagaService;
 
 	// Fitur 11
-	@RequestMapping(value = "lab/jadwal-jaga/tambah", method = RequestMethod.GET)
+	@GetMapping(value = "lab/jadwal-jaga/tambah")
 	private String addJadwalJaga(Model model) {
 		// Consume API
 		StaffResponse response = restTemplate.getForObject(Setting.getAllStaffLabUrl, StaffResponse.class);
@@ -47,23 +48,24 @@ public class JadwalJagaController {
 		return "jadwalJaga-add";
 	}
 
-	@RequestMapping(value = "lab/jadwal-jaga/tambah", method = RequestMethod.POST)
+	@PostMapping(value = "lab/jadwal-jaga/tambah")
 	private String addJadwalJagaSubmit(@ModelAttribute JadwalJagaModel jadwalJaga) {
 		jadwalJagaService.addJadwal(jadwalJaga);
 		return "jadwalJaga-add";
 	}
 
 	// Fitur 12
-	@RequestMapping(value = "lab/jadwal-jaga/{tanggal}", method = RequestMethod.GET)
+	@GetMapping(value = "lab/jadwal-jaga/{tanggal}")
 	private String getJadwalJaga(@PathVariable("tanggal") Date tanggal, Model model) {
 		List<JadwalJagaModel> archive = jadwalJagaService.getJadwalByDate(tanggal);
 		model.addAttribute("jadwalJaga", archive);
+		model.addAttribute("tanggal",tanggal);
 		model.addAttribute("title", "Lihat Jadwal Jaga");
 		return "jadwalJaga-view";
 	}
 
 	// Fitur 13
-	@RequestMapping(value = "lab/jadwal-jaga/ubah/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "lab/jadwal-jaga/ubah/{id}")
 	private String updateJadwalJaga(@PathVariable(value = "id") long id, Model model) {
 		JadwalJagaModel archive = jadwalJagaService.getJadwalById(id);
 		model.addAttribute("jadwalJaga", archive);
@@ -71,7 +73,7 @@ public class JadwalJagaController {
 		return "jadwalJaga-update";
 	}
 
-	@RequestMapping(value = "lab/jadwal-jaga/ubah/{id}", method = RequestMethod.POST)
+	@PostMapping(value = "lab/jadwal-jaga/ubah/{id}")
 	private String updateLabSupplySubmit(@PathVariable(value = "id") long id, JadwalJagaModel jadwalJaga, Model model) {
 		jadwalJaga.setId(id);
 		jadwalJagaService.updateJadwal(jadwalJaga);
@@ -80,6 +82,16 @@ public class JadwalJagaController {
 		return "success";
 	}
 	
+	//Menu Awal
+	@RequestMapping(value="/lab/jadwal-jaga", method = RequestMethod.GET)
+	private String viewAllJadwalJaga(Model model) {
+		List<JadwalJagaModel> jadwalJaga = jadwalJagaService.getAllJadwaJaga();
+		model.addAttribute("jadwalJaga", jadwalJaga);
+		model.addAttribute("title", "Jadwal Jaga Lab");	
+		return "jadwalJaga-home";
+	}
+
+	//Buat IGD
 	@ResponseBody
 	@GetMapping(value = "lab/jadwal-jaga/getJadwalJaga")
 	public List<JadwalJagaModel> getJadwalJagaPemeriksaan(Model model) {
