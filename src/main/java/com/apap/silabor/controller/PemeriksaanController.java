ede1ea8a7d78191be583f49a3b0ec1d2ab3b9efc
zidane zaffar <zidane.zaffar@gmail.com>
2018-12-10 00:35:38 +0700

    fixed feature 8

--- src/main/java/com/apap/silabor/controller/PemeriksaanController.java
+++ src/main/java/com/apap/silabor/controller/PemeriksaanController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.apap.silabor.model.PemeriksaanModel;
@@ -40,7 +41,7 @@ import com.apap.silabor.service.PemeriksaanService;
 import com.apap.silabor.service.SupplyService;
 
 @Controller
-@RequestMapping("/lab/pemeriksaan")
+//@RequestMapping("/lab/pemeriksaan")
 public class PemeriksaanController {
 //	@Autowired
 //	RestTemplate restTemplate;
@@ -83,7 +84,7 @@ public class PemeriksaanController {
 //	}
 
 	//FITUR 7 Menampilkan permintaan pemeriksaan lab
-	@GetMapping(value = "/permintaan")
+	@GetMapping(value = "/lab/pemeriksaan/permintaan")
 	public String viewAllPemeriksaan(Model model) throws IOException {
 		//Menambahkan Otentikasi Admin
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -137,19 +138,26 @@ public class PemeriksaanController {
 		model.addAttribute("title", "Daftar Pemeriksaan Lab");
 		return "pemeriksaan-viewall";
 	}
-//	//FITUR 8
-//    @PostMapping(value = "/permintaan/save")
-//    public BaseResponse<PemeriksaanModel> addLabResult(@RequestBody @Valid PemeriksaanModel pemeriksaan, BindingResult bindingResult) {
-//        BaseResponse<PemeriksaanModel> response = new BaseResponse<PemeriksaanModel>();
-//        pemeriksaanService.addPemeriksaan(pemeriksaan);
-//        response.setStatus(200);
-//        response.setMessage("success");
-//        response.setResult(pemeriksaan);
-//        return response;
-//    }
+	//FITUR 8
+	@ResponseBody
+    @PostMapping(value = "/api/lab/pemeriksaan/permintaan")
+    public BaseResponse<PemeriksaanModel> addLabResult(@RequestBody @Valid PemeriksaanModel pemeriksaan, BindingResult bindingResult) {
+        BaseResponse<PemeriksaanModel> response = new BaseResponse<PemeriksaanModel>();
+        if (bindingResult.hasErrors()) {
+            response.setStatus(500);
+            response.setMessage("error data");
+        }else {
+        	pemeriksaanService.addPemeriksaan(pemeriksaan);
+            response.setStatus(200);
+            response.setMessage("success");
+            response.setResult(pemeriksaan);
+
+        }
+              return response;
+    }
 
 	//FITUR 9
-	@PostMapping(value = "/{id}")
+	@PostMapping(value = "/lab/pemeriksaan/{id}")
 	public String updateStatus(@PathVariable(value="id") long id, Model model) {
 		PemeriksaanModel pemeriksaan = pemeriksaanService.getPemeriksaanById(id);
 		List<SupplyModel> supplyChoosen = new ArrayList<>();
@@ -217,7 +225,7 @@ public class PemeriksaanController {
 	}
 
 	//FITUR 10
-	@PostMapping(value = "/kirim/{id}")
+	@PostMapping(value = "/lab/pemeriksaan/kirim/{id}")
 	public String kirimPemeriksaan(@PathVariable(value="id") long id) {
 		
 		//Get Pemeriksaan Selesai
